e0c9cf4906e0a61a406986f340721552822778b8
Emiry00 <emir-00@live.de>
2021-11-18 19:40:24 +0100

    added signal system

---
+++ Assets/Emir/Scripts/SignalListener.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SignalListener : MonoBehaviour
+{
+    public SignalObject signal;
+    public UnityEvent signalEvent;
+
+    private void OnEnable()
+    {
+        signal.RegisterListener(this);
+    }
+
+    private void OnDisable()
+    {
+        signal.DeRegisterListener(this);
+    }
+
+    public void OnSignalRaised()
+    {
+        signalEvent.Invoke();
+    }
+}

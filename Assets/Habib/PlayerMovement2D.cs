c096705c2d19ef8e61b9b98dfaa81f05562d6600
habibfrhn <47953415+habibfrhn@users.noreply.github.com>
2021-11-24 12:12:57 +0100

    Basic movement with crouch, jumping and camera movement updated

---
+++ Assets/Habib/PlayerMovement2D.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2D : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+
+    bool jump = false;
+
+    bool crouch = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+    
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    
+        if(Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }else if(Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //move character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch,jump);
+        jump=false;
+
+    }
+}

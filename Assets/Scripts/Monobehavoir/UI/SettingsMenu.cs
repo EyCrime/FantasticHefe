7ba9a95a110d5f9f64834103a81dff31d33c4abf
Emircan Y <emir-00@live.de>
2021-12-05 16:18:03 +0100

    added settings menu

---
+++ Assets/Scripts/Monobehavoir/UI/SettingsMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public void SetVolume (float volume)
+    {
+        
+    }
+}

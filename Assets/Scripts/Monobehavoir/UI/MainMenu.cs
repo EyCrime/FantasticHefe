6f0e69ae41924f97ac55415219a55f9cd56a8f0b
Fatlind Krasniqi <s0564648@htw-berlin.de>
2021-12-09 23:23:35 +0100

    comments removed lol

--- Assets/Scripts/Monobehavoir/UI/MainMenu.cs
+++ Assets/Scripts/Monobehavoir/UI/MainMenu.cs
@@ -10,8 +10,6 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        
-
     }
 
     public void QuitGame()

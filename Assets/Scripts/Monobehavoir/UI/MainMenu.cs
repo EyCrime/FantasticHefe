7d44ce3ab5fa00c3e733033ce1d0f7914fd9e621
Fatlind Krasniqi <s0564648@htw-berlin.de>
2021-12-09 21:33:28 +0100

    Music plays continously

--- Assets/Scripts/Monobehavoir/UI/MainMenu.cs
+++ Assets/Scripts/Monobehavoir/UI/MainMenu.cs
@@ -6,21 +6,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public AudioSource ingameMusic;
-
-    private void Start()
-    {
-        ingameMusic.Play();
-    }
-    private void Awake()
-    {
-        DontDestroyOnLoad(transform.gameObject);
-        ingameMusic = GetComponent<AudioSource>();
-    }
 
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);    
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        
+
     }
 
     public void QuitGame()

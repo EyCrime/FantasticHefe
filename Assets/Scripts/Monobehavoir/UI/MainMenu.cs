d3ed15fb50fe84700861259e8eb5dc637755791d
Fatlind Krasniqi <s0564648@htw-berlin.de>
2021-12-09 19:55:57 +0100

    Changed animation/spritesheet and added background music + volume changer

--- Assets/Scripts/Monobehavoir/UI/MainMenu.cs
+++ Assets/Scripts/Monobehavoir/UI/MainMenu.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioSource ingameMusic;
+
+    private void Start()
+    {
+        ingameMusic.Play();
+    }
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        ingameMusic = GetComponent<AudioSource>();
+    }
+
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);    
     }
 
     public void QuitGame()

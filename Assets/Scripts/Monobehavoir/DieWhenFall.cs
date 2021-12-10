42ce97d7c468cc47fdf9d694423f3773403c4028
Emircan Y <emir-00@live.de>
2021-12-10 23:59:26 +0100

    final

--- Assets/Scripts/Monobehavoir/DieWhenFall.cs
+++ Assets/Scripts/Monobehavoir/DieWhenFall.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class DieWhenFall : MonoBehaviour
 {
     [SerializeField] private SignalObject gameOverSignal;
+    [SerializeField] private AudioSource deathSound;
+
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.CompareTag("Player"))
+        {
             gameOverSignal.Raise();
+            deathSound.Play();
+        }
     }
 }
